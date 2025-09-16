2254ded017b9038cd12b8fbcf520253fe5fa7338
[dyad] <git@dyad.sh>
2025-09-16 14:25:49 -0300

    Reverted all changes back to version c221ac6b9db87d9cb3b2879cced619fa9e5763d5

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-"use client";
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import ServiceSelectionPage from "./pages/ServiceSelectionPage";
-import CalendarPage from "./pages/CalendarPage";
-import AdminPanelPage from "./pages/AdminPanelPage"; // Importar a nova página
-import { Toaster } from "sonner";
+import NotFound from "./pages/NotFound";
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/services" element={<ServiceSelectionPage />} />
-        <Route path="/calendar" element={<CalendarPage />} />
-        <Route path="/admin" element={<AdminPanelPage />} /> {/* Nova rota para o painel de admin */}
-      </Routes>
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
       <Toaster />
-    </Router>
-  );
-}
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
 
 export default App;
