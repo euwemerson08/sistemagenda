ec6e52cfab452e56a2863218c471320fc539fa46
[dyad] <agapiceoficial@gmail.com>
2025-09-19 23:15:21 -0300

    [dyad] Adicionei um `console.log` em `src/main.tsx` para depurar a inicialização da aplicação. - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 
+console.log("main.tsx está sendo executado!"); // Adicionado para depuração
+
 createRoot(document.getElementById("root")!).render(<App />);
